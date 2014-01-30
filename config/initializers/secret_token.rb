c97a7a5c48e26751cb188d43fab4273f1fafe022
Sara Bouchikhi <sara1bouchikhi@gmail.com>
2014-01-30 18:45:39 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MON::Application.config.secret_key_base = '219e65dbe32e8f45fd71f125850483cb2e81c2485257f0ca0209290049c83587974fb4cbf8d57b71cbc9d4c7c5ef20fb038065ebee62cacbb668b91fc9e9e170'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MON::Application.config.session_store :cookie_store, key: '_MON_session'
